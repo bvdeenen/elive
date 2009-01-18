247907e2e317a519762b597c0d28526d3c8780c1
Bart van Deenen <bvdeenen@acer.(none)>
2009-01-18 14:43:09 +0100

    just a file, doesn't work

---
+++ one.erl
@@ -0,0 +1,23 @@
+%% ---
+%%  Excerpted from "Programming Erlang",
+%%  published by The Pragmatic Bookshelf.
+%%  Copyrights apply to this code. It may not be used to create training material, 
+%%  courses, books, articles, and the like. Contact us if you are in doubt.
+%%  We make no guarantees that this code is fit for any purpose. 
+%%  Visit http://www.pragmaticprogrammer.com/titles/jaerlang for more book information.
+%%---
+-module(one).
+-export([start/0]).
+
+start() -> 
+	     spawn(fun() -> counter(0) end).
+
+counter(N) -> 
+	io:format("hi ~g ~n", [N]),
+    receive    
+		cancel->
+			void
+		after 500 ->
+			counter(N+1)
+    end.
+
