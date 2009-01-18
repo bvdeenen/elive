fb8a1a709bca9773bf806501f503754679a348ad
Bart van Deenen <bvdeenen@acer.(none)>
2009-01-18 17:52:16 +0100

    kill die dead

--- one.erl
+++ one.erl
@@ -6,18 +6,38 @@
 -export([start/0]).
 
 start() -> 
-	     spawn(fun() -> counter(0) end).
+	 OnePid= spawn(fun() -> counter(startup) end),
+	 spawn(fun() -> loop(OnePid) end).
+
+% message handler
+loop(OnePid) ->
+	receive
+		die -> 
+			io:format("one ~p is killed~n", [self()] ),
+			OnePid ! die;
+		Any ->
+			io:format("one ~p receive ~p~n", [self(), Any]),
+			loop(OnePid)
+	end.
 
 cancel(Pid) -> Pid ! cancel.
 
-counter(10) -> void; 
+counter(10) -> 
+	void; 
+
+counter(startup) ->
+	world ! {birth, self() },
+	counter(0) ;
+
 counter(N) -> 
-	io:format("hi ~p ~n", [N]),
+	% talk to the world
+	io:format("one:counter ~p ~p~n", [self(), N]),
     receive    
-		cancel->
-			void;
-		anything ->
-			io:format("stopping~n"),
+		die ->
+			io:format("received die message~n");
+
+		Message ->
+			io:format("stopping for Message ~p~n", [Message] ),
 			void
 		after 1500 ->
 			counter(N+1)
--- world.erl
+++ world.erl
@@ -3,29 +3,46 @@
 -compile(export_all).
 
 start(N) ->
-	Pid = spawn(fun world:loop/0),
+	Pid = spawn(fun() -> loop([]) end),
 	register( world, Pid),
 	start_one(N),
 	Pid.
 
 rpc(Pid, Request) ->
+	%io:format("world:rpc(~p,~p)~n", [Pid, Request]),
 	Pid ! {self(), Request},
 	receive
 		{Pid, Response} ->
-			Response
+			{'World responds', Response}
 	end.		
 
-loop() ->
+loop(Beasts) ->
 	receive 
+		{birth, Pid} ->
+			io:format("new birth of ~p~n", [Pid]),
+			loop([Pid|Beasts]);
+		
+		die ->
+			io:format("world is dead~n", []),
+			kill_beasts(Beasts);
 		Any ->
 			io:format("world received ~p~n", [Any]),
-			loop()
+			loop(Beasts)
 	end.
+
+kill_beasts([]) ->
+	io:format("all beasts are dead~n");
+
+kill_beasts(Beasts) ->
+	[Pid|Rest] = Beasts,
+	Pid ! die,
+	kill_beasts(Rest).
 
 start_one(0) -> void;
 
 start_one(N) ->
 	io:format("starting beest ~p ~n", [N]),
-	spawn( one, start, []),
+	Pid = spawn( one, start, []),
+	io:format("beest ~p started with pid ~p", [N, Pid]),
 	start_one(N-1) .
 
