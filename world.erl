9d4bb5e871015dcb9f3e1351caee7fd67308fd9c
Bart van Deenen <bvdeenen@acer.(none)>
2009-01-18 16:48:53 +0100

    communication with world

--- world.erl
+++ world.erl
@@ -1,11 +1,31 @@
 
 -module(world).
--export([start/1]).
-
-start(0) -> void;
+-compile(export_all).
 
 start(N) ->
+	Pid = spawn(fun world:loop/0),
+	register( world, Pid),
+	start_one(N),
+	Pid.
+
+rpc(Pid, Request) ->
+	Pid ! {self(), Request},
+	receive
+		{Pid, Response} ->
+			Response
+	end.		
+
+loop() ->
+	receive 
+		Any ->
+			io:format("world received ~p~n", [Any]),
+			loop()
+	end.
+
+start_one(0) -> void;
+
+start_one(N) ->
 	io:format("starting beest ~p ~n", [N]),
-	spawn( fun() -> one:start() end),
-	start(N-1) .
+	spawn( one, start, []),
+	start_one(N-1) .
 
